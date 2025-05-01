8cc0ca5346589a9e209ceb58a76e245633695346
Stan-tab <godofwarmafia2@gmail.com>
2025-05-01 14:21:13 +0500

    Experiments

---
+++ src/greeting.jsx
@@ -0,0 +1,14 @@
+import "./greeting.css";
+
+export default function Greeting() {
+	return (
+		<>
+			<input type="text" />
+			<li>hey</li>
+			<h1 className="swearing">
+				&quot;I swear by my pretty floral bonnet, I will end you.&quot;
+			</h1>
+			<h1 className="swearing">&quot;Skibidi&quot;</h1>
+		</>
+	);
+}
